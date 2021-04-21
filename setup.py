bd4731a7864779e5113e8240f76c58ab5405ac55
Yaroslav Nikonorov <yaroslav.n@quali.com>
2021-04-21 14:12:35 +0300

    Added pre-commit

--- setup.py
+++ setup.py
@@ -34,11 +34,9 @@ setup(
     packages=find_packages(),
     install_requires=required,
     tests_require=required_for_tests,
-    python_requires=(
-        ">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, !=3.6.*, <4"
-    ),
+    python_requires="~=3.7",
     version=version_from_file,
     package_data={"": ["*.txt"]},
-    description="<your package description>",
+    description="Quali kubernetes specific package",
     include_package_data=True,
 )
